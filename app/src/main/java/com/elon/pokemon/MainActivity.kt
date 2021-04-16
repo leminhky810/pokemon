a21712887341fac7f6d8e6568761075a5e681860
Elon <minhky@brickmate.kr>
2021-04-16 15:28:03 +0700

    add base dependency

--- app/src/main/java/com/elon/pokemon/MainActivity.kt
+++ app/src/main/java/com/elon/pokemon/MainActivity.kt
@@ -2,6 +2,7 @@ package com.elon.pokemon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.HiltAndroidApp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
